800415d44efe40bbf93a51bbde8d19539c786609
YQisme <1398757912@qq.com>
2022-11-23 16:46:22 +0800

    demo

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,10 @@
+import DefaultTheme from "vitepress/theme";
+import Particles from "vue3-particles";
+
+export default {
+  ...DefaultTheme,
+  enhanceApp({ app }) {
+    app.use(Particles);
+  },
+};
+console.trace();
---
+++ docs/index.md
@@ -0,0 +1,4 @@
+<script setup>
+import myparticles from '@theme/myParticles.vue'
+</script>
+<myparticles/>
